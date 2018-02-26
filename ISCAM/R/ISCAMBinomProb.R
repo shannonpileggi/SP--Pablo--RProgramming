13c7b287942fd3e9b99940b377d6b217d23d2501
Noelle <noellebpablo@gmail.com>
2018-02-26 15:37:33 -0800

    updated iscambinomprob to utilize ggplot

---
+++ ISCAM/R/ISCAMBinomProb.R
@@ -0,0 +1,65 @@
+#' ISCAMBinomProb Function
+#'
+#' This function calculates tail probabilities from the binomial distribution.  
+#' @param k number of successes of interest (must be an integer)
+#' @param n number of trials (zero or more)
+#' @param prob probability of success on each trial
+#' @param lower.tail logical; a Boolean for finding the probability above (FALSE) or below (TRUE) the inputted value (inclusive) 
+#' @keywords binomial
+#' @export
+#' @examples
+#' ISCAMBinomProb(20, 30, 0.5, lower.tail = TRUE)
+#' ISCAMBinomProb(10, 55, 0.10, lower.tail = FALSE)
+
+ISCAMBinomProb <- function(k, n, prob, lower.tail){
+  myTitle <- substitute(paste("Binomial (", n==x1,", ", pi==x2, ")", ), list(x1=n, x2=prob)) #graph's main title
+  thisx <- 0:n #range of data (number of trials) 
+  if (lower.tail){
+    this.prob <- pbinom(k, n, prob) #calculating binomial probability
+    showprob <- format(this.prob, digits=4) #formatting probability to include only 4 sig. figs.
+    mySubtitle <- paste("P(X <=", k, ") =", showprob) #creating subtitle
+    df <- data.frame(x = thisx, y = dbinom(thisx, n, prob)) #putting data into data frame
+    plot1 <- ggplot(df, aes(x = x, y = y)) + 
+             geom_bar(stat = "identity", 
+                      col = "black", 
+                      fill = "grey",
+                      alpha = .2) + 
+             geom_bar(stat = "identity", #fills in part of histogram
+                      data = subset(df, x <= k), 
+                      colour="black", 
+                      fill="red",
+                      alpha = .7) +
+             labs(x = "Number of Successess",
+                  y = "Probability",
+                  title = myTitle, 
+                  subtitle = mySubtitle) + 
+            theme_bw(16, "serif") + 
+            theme(plot.subtitle=element_text(color="red"))
+    cat("Probability", k, "and below =", this.prob, "\n") #output the binom. prob.
+  }
+  if (!lower.tail){
+    this.prob <- 1 - pbinom(k-1, n, prob)
+    showprob <- format(this.prob, digits=4)
+    mySubtitle <- paste("P(X >=", k, ") =", showprob)
+    df <- data.frame(x = thisx, y = dbinom(thisx, n, prob))
+    plot1 <- ggplot(df, aes(x = x, y = y)) + 
+             geom_bar(stat = "identity", 
+                      col = "black", 
+                      fill = "grey",
+                      alpha = .2) + 
+             geom_bar(stat = "identity", 
+                     data = subset(df, x >= k), 
+                     colour="black", 
+                     fill="red",
+                     alpha = .7)+
+             labs(x = "Number of Successes",
+                  y = "Probability", 
+                  title = myTitle,
+                  subtitle = mySubtitle) + 
+             theme_bw(16, "serif") + 
+             theme(plot.subtitle=element_text(color="red"))
+    cat("Probability", k, "and above =", this.prob, "\n")
+  }
+  print(plot1)
+  return(this.prob)
+}
