dd030c4a7defa5a134c8f2e5dbebad6e2b4fac88
Noelle <noellebpablo@gmail.com>
2018-03-19 19:52:07 -0700

    removed unnecessary code/comments

--- ISCAM/R/ISCAMBinomTest.R
+++ ISCAM/R/ISCAMBinomTest.R
@@ -25,8 +25,6 @@ ISCAMBinomTest <- function(observed, n, hypothesized, alternative, conf.level = 
   showprob <- format(pvalue, digits = 4)
   
   if (alternative =="less"){
-    #this.prob <- pbinom(observed, n, hypothesized) #calculating binomial probability
-    #showprob <- format(this.prob, digits=4) #formatting probability to include only 4 sig. figs.
     mySubtitle <- paste("p-value =", showprob) #creating subtitle
     
     plot1 <- ggplot(df, aes(x = x, y = y, width = 0.25)) + 
@@ -47,8 +45,6 @@ ISCAMBinomTest <- function(observed, n, hypothesized, alternative, conf.level = 
       theme(plot.subtitle=element_text(color="#3366FF"))
   }
   else if (alternative =="greater"){
-    #this.prob <- 1 - pbinom(observed - 1, n, hypothesized)
-    #showprob <- format(this.prob, digits=4)
     mySubtitle <- paste("P-value =", showprob)
     plot1 <- ggplot(df, aes(x = x, y = y, width = 0.25)) + 
       geom_bar(stat = "identity", 
@@ -68,17 +64,6 @@ ISCAMBinomTest <- function(observed, n, hypothesized, alternative, conf.level = 
       theme(plot.subtitle=element_text(color="#3366FF"))
   }
   else if (alternative == "two.sided"){
-    
-    #all.probs<-dbinom(0:n, prob=hypothesized, size=n)
-    # probability that the outcome is x=2
-    #prob.x<-dbinom(x=observed, prob=hypothesized, size=n)
-    
-    # probability of all outcomes that have a probability lower than or equal to the probability of the outcome 2 (prob.x) 
-    #p.value<-sum(all.probs[all.probs <= prob.x])
-    #p.value
-    
-   
-    #showprob <- format(p.value, digits=4)
     mySubtitle <- paste("Two-sided p-value =", showprob)
     phat <- observed/n
     phatdiff <- abs(phat - hypothesized)
@@ -102,7 +87,6 @@ ISCAMBinomTest <- function(observed, n, hypothesized, alternative, conf.level = 
       theme(plot.subtitle=element_text(color="#007f80"))
   }
   print(plot1)
-  #return(c(upper, lower))
   cat("\n", "Exact Binomial Test\n", sep="","\n")
   statistic <- signif(observed/n, 4)
   cat(paste("Data: observed successes = ", observed, ", sample size = ", n, ", sample proportion = ", statistic, "\n\n", sep=""))
@@ -111,12 +95,5 @@ ISCAMBinomTest <- function(observed, n, hypothesized, alternative, conf.level = 
   cat(paste("Alternative hypothesis: pi", altname, hypothesized, sep=" "),"\n")
   cat(paste("p-value:", showprob, sep=" "), "\n")
   
-  #calculating the CI: 
-  #if (conf.level){
-   # phat <- observed/n
-    #lower <- phat - qnorm((1-conf.level)/2, lower.tail = F)*sqrt((1/n)*phat*(1-phat))
-    #upper <- phat + qnorm((1-conf.level)/2, lower.tail = F)*sqrt((1/n)*phat*(1-phat))
-    #cat(100*conf.level, "% Confidence interval for pi: (", lower,",", upper,")")
-  #}
   cat(100*conf.level, "% Confidence interval for pi:", CI)
 }
