ff917e12c6c2c33e9956e443be79a0304fef7507
Noelle <noellebpablo@gmail.com>
2018-04-30 11:50:10 -0700

    changed ISCAMSummary to include skimr package

---
+++ ISCAM/R/ISCAMSummary.R
@@ -0,0 +1,25 @@
+#' ISCAMSummary Function
+#'
+#' This function calculates the five number summary, mean, and standard deviation of the quantitative variable x.
+#' @param x quantitative variable
+#' @param explanatory optional, categorical grouping variable
+#' @param digits number of tick marks on x axis
+#' @keywords summary
+#' @export
+#' @examples
+#' ISCAMSummary(chickwts$weight, chickwts$feed, digits = 4)
+
+
+ISCAMSummary <- function(x, explanatory=NULL, digits = 3){
+  skim_format(numeric = list(digits = digits))
+  skim_with(numeric = list(p50 = NULL, p100 = NULL, p0 = NULL, 
+                           median = median, min = min, max = max))
+  if(is.null(explanatory)){
+    skim(x)
+  } else if (!is.null(explanatory)){
+    data <- data.frame(x = x, explanatory = explanatory)
+    data %>%
+      dplyr::group_by(explanatory) %>%
+      skim()
+  }
+}
